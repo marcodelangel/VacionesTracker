fe581c250a125dabb57370e7235eb25ec7ea0983
marcodelangel <marcodelangel@Marcos-MacBook-Air-2.local>
2017-12-23 12:18:06 -0600

    Cambios en SB

---
+++ VacionesTracker/ToDo.swift
@@ -0,0 +1,28 @@
+//
+//  ToDo.swift
+//  VacionesTracker
+//
+//  Created by Marco Del Angel on 12/22/17.
+//  Copyright © 2017 Marco Del Angel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ToDo {
+    var tittle : String
+    var isComplete : Bool
+    var dueDate :  Date
+    var notes : String?
+    var photos : [UIImage]?
+    
+    static func loadToDos() -> [ToDo]?{
+        return nil
+    }
+    
+    static func loadSampleTodos() -> [ToDo] {
+        let todo1 = ToDo(tittle: "One", isComplete: false, dueDate: Date(), notes: nil, photos: nil)
+        let todo2 = ToDo(tittle: "Two", isComplete: false, dueDate: Date(), notes: nil, photos: nil)
+        return[todo1, todo2]
+    }
+}
---
+++ VacionesTracker/ToDoTableViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ToDoTableViewController.swift
+//  VacionesTracker
+//
+//  Created by Marco Del Angel on 12/22/17.
+//  Copyright © 2017 Marco Del Angel. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var todos = [ToDo]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let savedToDos = ToDo.loadToDos() {
+            todos = savedToDos
+        } else {
+            todos = ToDo.loadSampleTodos()
+        }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return todos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = todos[indexPath.row].tittle
+
+        return cell
+    }
+
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+    @IBAction func unwindToDoTable(segue: UIStoryboardSegue){
+    
+    }
+    
+}
