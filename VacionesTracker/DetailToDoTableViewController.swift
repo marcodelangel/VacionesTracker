0cfd33a57e7bf82235491d3e2978ceb895267455
marcodelangel <marcodelangel@Marcos-MacBook-Air-2.local>
2017-12-23 12:28:25 -0600

    Add DetailTVC

---
+++ VacionesTracker/DetailToDoTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailToDoTableViewController.swift
+//  VacionesTracker
+//
+//  Created by Marco Del Angel on 12/23/17.
+//  Copyright © 2017 Marco Del Angel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailToDTableViewController : UITableViewController {
+    
+}
+
+
